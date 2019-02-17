c572ef9805b1343d0bd08b5f9edc1220fa470b37
Yuxin <956163372@qq.com>
2019-02-17 18:00:03 +0800

    首页布局

---
+++ components/public/header/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-row :gutter="0" class="m-header">
+    <el-col :span="6">
+      <geo/>
+    </el-col>
+    <el-col :span="6">
+      <user/>
+    </el-col>
+    <el-col :span="12">
+      <nav-bar></nav-bar>
+    </el-col>
+  </el-row>
+</template>
+<script>
+import Geo from "./geo";
+import User from "./user";
+import NavBar from "./nav";
+export default {
+  components: {
+    Geo,
+    User,
+    NavBar
+  }
+};
+</script>
+<style lang="scss">
+@import "@/assets/css/public/layout.scss";
+@import "@/assets/css/public/header/index.scss";
+</style>
